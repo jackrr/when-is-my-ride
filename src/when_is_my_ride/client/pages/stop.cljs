c5b2dc953921734c716b0749fa0dda36738c3874
Jack <jackratner@gmail.com>
2022-02-15 17:20:41 -0500

    Add rudimentary arrival UI
    Displays ordered list of arrivals from backend on stop page.

--- src/when_is_my_ride/client/pages/stop.cljs
+++ src/when_is_my_ride/client/pages/stop.cljs
@@ -1,12 +1,37 @@
 (ns when-is-my-ride.client.pages.stop
   (:require [clojure.string :as str]
             [re-frame.core :as rf]
-            [when-is-my-ride.client.api :as api]))
+            [when-is-my-ride.client.api :as api]
+            [when-is-my-ride.client.route :as route]))
+
+(rf/reg-sub ::stop (fn [{::keys [stop]}] (or stop {:arrivals []
+                                                   :routes {}})))
+(rf/reg-sub ::stop-name
+            (fn [_ _] (rf/subscribe [::stop]))
+            (fn [stop _] (:name stop)))
+(rf/reg-sub ::arrivals
+            (fn [_ _] (rf/subscribe [::stop]))
+            (fn [stop _] (:arrivals stop)))
+(rf/reg-sub ::routes
+            (fn [_ _] (rf/subscribe [::stop]))
+            (fn [stop _] (:routes stop)))
+(rf/reg-sub ::next-n-arrivals
+            (fn [_ _] [(rf/subscribe [::routes])
+                       (rf/subscribe [::arrivals])])
+            (fn [[routes arrivals] [_ n]]
+              (->> arrivals
+                  (sort-by :at)
+                  (take n)
+                  (map (fn [arr] (assoc arr :route (get routes (:route-id arr))))))))
 
 (rf/reg-event-db
  ::handle-stop-result
- (fn [db [_ data]]
-   (println "Got stop data" data)))
+ (fn [db [_ stop]]
+   (assoc db ::stop {:id (:id stop)
+                     :name (:name stop)
+                     :routes (reduce (fn [rs r]
+                                       (assoc rs (:id r) r)) {} (:routes stop))
+                     :arrivals (:arrivals stop)})))
 
 (rf/reg-event-fx
  ::load-stop
@@ -14,12 +39,24 @@
    {:dispatch [::api/fetch {:path (str "/stops/" stop-id)
                             :on-success [::handle-stop-result]}]}))
 
+(defn arrival [now {:keys [id route at direction]}]
+  [:div {:key id :className "my-2 flex gap-4"}
+   (route/icon route)
+   [:p direction]
+   [:p (let [min (-> at
+                    (* 1000)
+                    (- now)
+                    (/ (* 60 1000))
+                    (js/Math.floor))]
+         (if (> 1 min) "Now" (str min " min")))]])
+
 (defn view [_]
-  [:div "Stop"])
+  [:div {:className "mx-auto max-w-md"}
+   [:h2 {:className "text-xl mb-4"} @(rf/subscribe [::stop-name])]
+   (map (partial arrival (.valueOf (js/Date.))) @(rf/subscribe [::next-n-arrivals 20]))])
 
 (def route {:name :stop
             :path "/stops/:stop-id"
             :view view
             :on-load (fn [{:keys [params]}]
-                       (println params)
                        (rf/dispatch [::load-stop (:stop-id params)])) })
