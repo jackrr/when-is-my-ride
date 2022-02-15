16531ca621ab05f73b7e1abc3fe6c4c8b11b0bab
Jack <jackratner@gmail.com>
2022-02-15 18:38:33 -0500

    fix: just run server
    App invocation is a simple server start.

--- src/when_is_my_ride/core.clj
+++ src/when_is_my_ride/core.clj
@@ -1,10 +1,9 @@
 (ns when-is-my-ride.core
   (:gen-class)
-  (:require [systemic.core :as systemic]
-            [when-is-my-ride.server]))
+  (:require [ring.adapter.jetty :as jetty]
+            [when-is-my-ride.server :as server]))
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!")
-  (systemic/start!))
+  [& _]
+  (jetty/run-jetty #'server/app {:port 3000, :join? false, :async? true}))
