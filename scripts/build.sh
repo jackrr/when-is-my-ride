e2ca256eccf7dfd29ab7d833012a66d186bd4485
Jack Ratner <jackratner@gmail.com>
2025-08-27 13:48:10 -0400

    build for arm64

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,5 +6,5 @@ VERSION=$(./scripts/version.sh)
 IMAGE=jackratner/when-is-my-ride:${VERSION}
 npx shadow-cljs release :app
 lein uberjar
-docker build . -t ${IMAGE}
+docker buildx build --platform linux/amd64,linux/arm64 . -t ${IMAGE}
 docker push ${IMAGE}
